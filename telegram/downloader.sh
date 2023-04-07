fdf38dff8edb9be44f6a7d0a1bc4b89a793ae343
Ketanest <87452756+Ketanest@users.noreply.github.com>
2023-04-07 13:24:09 +0200

    Update downloader.sh

--- telegram/downloader.sh
+++ telegram/downloader.sh
@@ -6,5 +6,15 @@ set -x
 # get 
 curl -s https://core.telegram.org/resources/cidr.txt > /tmp/telegram.txt
 
+# seperate IPv4 and IPv6
+while read line; do
+  if [[ $line == *:* ]]; then
+    echo "$line" >> /tmp/telegram-ipv6.txt
+  else
+    echo "$line" >> /temp/telegram-ipv4.txt
+  fi
+done < /tmp/telegram.txt
+
 # sort & uniq
-sort -h /tmp/telegram.txt | uniq > telegram/ipv4.txt
+sort -h /tmp/telegram-ipv4.txt | uniq > telegram/ipv4.txt
+sort -h /tmp/telegram-ipv6.txt | uniq > telegram/ipv6.txt
