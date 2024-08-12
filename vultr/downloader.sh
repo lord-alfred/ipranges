222d4ec6eecd2e9d8b3924b1d7ab902008d7f3d5
Ketanest <87452756+Ketanest@users.noreply.github.com>
2024-08-12 23:54:58 +0200

    Update downloader.sh

--- vultr/downloader.sh
+++ vultr/downloader.sh
@@ -8,15 +8,15 @@ set -x
 
 
 # get from public ranges
-curl -s https://geofeed.constant.com/?json > /tmp/vultr.json
+curl -s https://geofeed.constant.com/?text > /tmp/vultr.txt
 
+#save ipv4
+REGEX_V4="^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)(\/([1-9]|[1-2][0-9]|3[0-2])){0,1}$"
+grep -E $REGEX_V4 /tmp/vultr.txt > /tmp/vultr-ipv4.txt
 
-# save ipv4
-jq '.prefixes[] | [.ip_prefix][] | select(. != null)' -r /tmp/vultr.json > /tmp/vultr-ipv4.txt
-
-# save ipv6
-jq '.ipv6_prefixes[] | [.ipv6_prefix][] | select(. != null)' -r /tmp/vultr.json > /tmp/vultr-ipv6.txt
-
+#save ipv6
+REGEX_V6="([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})"
+grep -E $REGEX_V6 /tmp/vultr.txt > /tmp/vultr-ipv6.txt
 
 # sort & uniq
 sort -V /tmp/vultr-ipv4.txt | uniq > vultr/ipv4.txt
