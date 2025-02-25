69fe4035a72d5807fe20080670b661a55858da74
Lord Alfred <2259979+lord-alfred@users.noreply.github.com>
2025-02-25 23:17:09 +0300

    Add new providers (#22)
    * Add Apple proxy
    
    * Add ProtonVPN
    
    * Fix download OpenAI 
    
    * Improve workflow

--- openai/downloader.sh
+++ openai/downloader.sh
@@ -8,12 +8,37 @@ set -x
 
 
 # get from public ranges
-curl -s https://openai.com/gptbot-ranges.txt > /tmp/openai.txt
+download_and_parse_json() {
+    curl --connect-timeout 60 --retry 3 --retry-delay 15 -s "${1}" \
+    -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+    -H 'accept-language: en' \
+    -H 'cache-control: no-cache' \
+    -H 'pragma: no-cache' \
+    -H 'priority: u=0, i' \
+    -H 'sec-ch-ua: "Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"' \
+    -H 'sec-ch-ua-mobile: ?0' \
+    -H 'sec-ch-ua-platform: "macOS"' \
+    -H 'sec-fetch-dest: document' \
+    -H 'sec-fetch-mode: navigate' \
+    -H 'sec-fetch-site: none' \
+    -H 'sec-fetch-user: ?1' \
+    -H 'upgrade-insecure-requests: 1' \
+    -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36' \
+    > /tmp/openai.json
 
-# save ipv4
-grep -v ':' /tmp/openai.txt > /tmp/openai-ipv4.txt
+    jq '.prefixes[] | [.ipv4Prefix][] | select(. != null)' -r /tmp/openai.json > /tmp/openai.txt
 
-# ipv6 not provided
+    # save ipv4
+    grep -v ':' /tmp/openai.txt >> /tmp/openai-ipv4.txt
+
+    # ipv6 not provided
+
+    sleep 10
+}
+
+download_and_parse_json "https://openai.com/chatgpt-user.json"
+download_and_parse_json "https://openai.com/searchbot.json"
+download_and_parse_json "https://openai.com/gptbot.json"
 
 
 # sort & uniq
